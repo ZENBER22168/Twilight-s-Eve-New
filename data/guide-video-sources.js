1749e61b26abd3aabab6a3835ab7e6bb440b8cd2
ZENBER22168 <s68012888@gmail.com>
2026-09-13 01:04:27 +0800

    更新影片

---
+++ data/guide-video-sources.js
@@ -0,0 +1,11 @@
+window.GUIDE_VIDEO_SOURCES = {
+  imp1: null,
+  imp2: null,
+  imp3: null,
+  m1: {
+    youtubeId: "u1ywnttmbWM",
+    embedHost: "https://www.youtube.com",
+    provider: "il108allen",
+    url: "https://youtu.be/u1ywnttmbWM"
+  }
+};
